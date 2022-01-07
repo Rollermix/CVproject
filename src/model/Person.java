59628e72a9d9aff9ba0685f440eae185d80a0085
Rollermix <p.czajka.1998@gmail.com>
2022-01-07 20:53:01 +0100

    Revert "Add gradle and lombok dependency"
    This reverts commit a7637aa59f8ed78481ca5baedcee9449f70d42cb.

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,4 +1,5 @@
 package model;
+
 public class Person {
     private String firstName;
     private String lastName;
--- src/model/Subject.java
+++ src/model/Subject.java
@@ -1,12 +1,37 @@
 package model;
-@GetteR
-import model.enumeration.FieldOfStudy;
 
 public class Subject {
     private String subjectName;
     private int amountOfECTS;
-    private FieldOfStudy fieldOfStudy;
+    private String fieldOfStudy;
 
+    public String getSubjectName() {
+        return subjectName;
+    }
 
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
 
+    public int getAmountOfECTS() {
+        return amountOfECTS;
+    }
+
+    public void setAmountOfECTS(int amountOfECTS) {
+        this.amountOfECTS = amountOfECTS;
+    }
+
+    public String getFieldOfStudy() {
+        return fieldOfStudy;
+    }
+
+    public void setFieldOfStudy(String fieldOfStudy) {
+        this.fieldOfStudy = fieldOfStudy;
+    }
+
+    public Subject(String subjectName, int amountOfECTS, String fieldOfStudy) {
+        this.subjectName = subjectName;
+        this.amountOfECTS = amountOfECTS;
+        this.fieldOfStudy = fieldOfStudy;
+    }
 }
